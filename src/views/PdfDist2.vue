532415c8baa38e3038aac02277e94d516b52dcdd
LouisKo <louiskoxiang@gmail.com>
2021-10-26 14:21:17 +0800

    Get PDf ArrayBuffer type

--- src/views/PdfDist2.vue
+++ src/views/PdfDist2.vue
@@ -10,6 +10,11 @@
 import { defineComponent, ref } from 'vue'
 import ObjectPdf from '@/components/ObjectPdf.vue' // @ is an alias to /src
 
+// const request = axios.create({
+//   baseURL: 'http://localhost:3000/',
+//   transformResponse: (r: ServerResponse) => r.data
+// })
+
 export default defineComponent({
   name: 'pdf',
   components: {
@@ -19,9 +24,17 @@ export default defineComponent({
     const bindUrl = ref('')
     const renderComponent = ref(false)
     const componentKey = ref(0)
-    const changePdf1 = () => {
+    const changePdf1 = async () => {
       componentKey.value += 1
-      bindUrl.value = 'http://127.0.0.1:5500/src/assets/pdf/X.pdf'
+      const pdfFileToArrayBuffer = await fetch('http://127.0.0.1:5500/src/assets/pdf/XD2.pdf')
+      const buffer = await pdfFileToArrayBuffer.arrayBuffer()
+      console.log(buffer, buffer instanceof ArrayBuffer)
+      const bufferToUrl = URL.createObjectURL(
+        new Blob([buffer], {
+          type: 'application/pdf'
+        })
+      )
+      bindUrl.value = bufferToUrl
       // forceRerender()
     }
     const changePdf2 = () => {
@@ -29,6 +42,7 @@ export default defineComponent({
       bindUrl.value = 'http://127.0.0.1:5500/src/assets/pdf/XD2.pdf'
       // forceRerender()
     }
+
     // const forceRerender = () => {
     //   // Remove my-component from the DOM
     //   renderComponent.value = false
@@ -38,6 +52,7 @@ export default defineComponent({
     //     renderComponent.value = true
     //   })
     // }
+
     return {
       bindUrl,
       changePdf1,
